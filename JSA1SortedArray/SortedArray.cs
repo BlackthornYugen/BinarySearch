96dbbe03b394bd28ca09db8f8414dbbcb14d3dd6
John Steel <jsteel-cc@conestogac.on.ca>
2015-02-03 19:57:19 -0500

    2 of 4 tests pass

--- JSA1SortedArray/SortedArray.cs
+++ JSA1SortedArray/SortedArray.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace JSA1SortedArray
 {
-    public class SortedArray
+    public class SortedArray<T>
     {
+        private T[] _storedTypes;
+        private int _pointer = 0;
+
+        public SortedArray(int maxN)
+        {
+            _storedTypes = new T[maxN];
+        }
+
+        public T this[int index]
+        {
+            get { return _storedTypes[index]; }
+        }
+
+        public void Insert(T arg)
+        {
+            _storedTypes[_pointer++] = arg;
+        }
     }
 }
---
+++ SortedArrayTests/SATestSuite01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JSA1SortedArray;
+using NUnit.Framework;
+
+namespace SortedArrayTests
+{
+
+    public class SATestSuite01
+    {
+        [TestCase("B", 0)]
+        [TestCase("BD", 1)]
+        [TestCase("BDC", 1)]
+        [TestCase("BDCA", 0)]
+        public void DivideTest(String s, int i)
+        {
+            var sa = new SortedArray<char>(4);
+            char c = default(char);
+            foreach (char t in s)
+            {
+                c = t;
+                sa.Insert(c);
+            }
+            Assert.AreEqual(c, sa[i]);
+        }
+
+
+    }
+}

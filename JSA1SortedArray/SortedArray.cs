85764cc884472edf7b29549120aba7d2e6d4b02b
John Steel <jsteel-cc@conestogac.on.ca>
2015-02-03 20:44:29 -0500

    7 of 11 tests pass

--- JSA1SortedArray/SortedArray.cs
+++ JSA1SortedArray/SortedArray.cs
@@ -8,22 +8,54 @@ namespace JSA1SortedArray
 {
     public class SortedArray<T>
     {
-        private T[] _storedTypes;
-        private int _pointer = 0;
+        private IComparable<T>[] _storedTypes;
+        private int _tail;
 
         public SortedArray(int maxN)
         {
-            _storedTypes = new T[maxN];
+            _storedTypes = new IComparable<T>[maxN];
         }
 
         public T this[int index]
         {
-            get { return _storedTypes[index]; }
+            get { return (T)_storedTypes[index]; }
+        }
+
+        private int BinarySearch(IComparable<T> arg)
+        {
+            int low = 0;
+            int high = _tail;
+
+            while (low < high)
+            {
+                int midpoint = (high - low) / 2 + low;
+                int compareResult = _storedTypes[midpoint].CompareTo((T) arg);
+
+                if (compareResult > 0)
+                    low = midpoint + 1;
+                else
+                    high = midpoint;
+            }
+
+            if ((low == high) && (arg == _storedTypes[low]))
+                return low;
+            return -1;
         }
 
         public void Insert(T arg)
         {
-            _storedTypes[_pointer++] = arg;
+            if (null == arg)
+                throw new ArgumentNullException("arg");
+            if (IsEmpty())
+                _storedTypes[0] = (IComparable<T>) arg;
+            else
+                _storedTypes[BinarySearch((IComparable<T>) arg)] = (IComparable<T>)arg;
+            _tail++;
+        }
+
+        public bool IsEmpty()
+        {
+            return _tail == 0;
         }
     }
 }
--- SortedArrayTests/SATestSuite01.cs
+++ SortedArrayTests/SATestSuite01.cs
@@ -15,7 +15,7 @@ namespace SortedArrayTests
         [TestCase("BD", 1)]
         [TestCase("BDC", 1)]
         [TestCase("BDCA", 0)]
-        public void DivideTest(String s, int i)
+        public void SortPositionTest(String s, int i)
         {
             var sa = new SortedArray<char>(4);
             char c = default(char);
@@ -27,6 +27,59 @@ namespace SortedArrayTests
             Assert.AreEqual(c, sa[i]);
         }
 
+        [Test]
+        public void TestEmpty()
+        {
+            var sa = new SortedArray<string>(4);
+            Assert.IsTrue(sa.IsEmpty(), "IsEmpty didn't return True after construction.");
+        }
 
+        [Test]
+        public void TestNotEmpty()
+        {
+            var sa = new SortedArray<string>(4);
+            sa.Insert("One Thing");
+            Assert.IsFalse(sa.IsEmpty(), "IsEmpty returned True after insert.");
+        }
+
+        [Test]
+        public void TestFull()
+        {
+            var sa = new SortedArray<string>(4);
+            sa.Insert("One Thing");
+            sa.Insert("Two Thing");
+            sa.Insert("Three Thing");
+            sa.Insert("Four Thing");
+            Assert.IsFalse(sa.IsEmpty(), "IsEmpty returned True after insert.");
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestNullInsert()
+        {
+            var sa = new SortedArray<string>(4);
+            sa.Insert(null);
+        }
+
+        [Test]
+        public void TestIntOne()
+        {
+            var sa = new SortedArray<int>(4);
+            sa.Insert(1);
+        }
+
+        [Test]
+        public void TestIntNegativeOne()
+        {
+            var sa = new SortedArray<int>(4);
+            sa.Insert(1);
+        }
+
+        [Test]
+        public void TestIntZero()
+        {
+            var sa = new SortedArray<int>(4);
+            sa.Insert(0);
+        }
     }
 }
